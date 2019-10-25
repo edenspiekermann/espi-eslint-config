b616dd58ade0a55fd334efbe9dc2f8e083af23f3
Norman Rusch <me@nrmnrsh.com>
2019-10-25 11:56:35 +0200

    Fix unparsable typescript-react config

--- rules/typescript-react.js
+++ rules/typescript-react.js
@@ -1,21 +1,31 @@
-const typescript = require('./typescript');
-
 module.exports = {
   overrides: [
     {
-      ...typescript,
       files: ['*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      parser: '@typescript-eslint/parser',
       parserOptions: {
+        project: './tsconfig.json',
         ecmaFeatures: {
           jsx: true,
         },
-        project: './tsconfig.json',
       },
+      plugins: [
+        '@typescript-eslint',
+      ],
       settings: {
         react: {
           pragma: 'React',
           version: 'detect',
         },
+      },
+      rules: {
+        'react/jsx-filename-extension': ['error', {
+          extensions: ['.tsx'],
+        }],
       },
     },
   ],
